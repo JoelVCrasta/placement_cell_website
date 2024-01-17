c8693257b182a966ff19447a7ef5fde8209bb120
Joel Crasta <loejstarc@gmail.com>
2024-01-17 19:10:15 +0530

    skeleton react

--- client/src/app/drive/[id]/page.jsx
+++ client/src/app/drive/[id]/page.jsx
@@ -4,8 +4,27 @@ import RolesCard from "@/components/RolesCard";
 import Data from "../../../../public/data";
 import { usePathname } from "next/navigation";
 import { notFound } from "next/navigation";
+import { useState, useEffect, useRef } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const driveinfo = () => {
+  const [boxWidth, setBoxWidth] = useState(0);
+  const widthRef = useRef(null);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      if (widthRef.current) {
+        setBoxWidth(widthRef.current.offsetWidth);
+      }
+    };
+    updateWidth();
+    const intervalId = setInterval(updateWidth, 500);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  console.log(boxWidth);
+
   const pathName = usePathname();
   const pathNo = pathName.slice("/drive/".length);
   const dataAll = Data.find((item) => item.id === Number(pathNo));
@@ -15,37 +34,53 @@ const driveinfo = () => {
   }
 
   return (
-    <div className="flex items-center justify-center py-10 mb-10 h-auto bg-background-clr font-inter font-normal">
-      <section className="flex flex-col p-5 sm:p-8 lg:p-16  w-11/12 sm:w-10/12 md:w-2/3 lg:w-3/5 border-white h-auto rounded-md bg-primary-card">
-        <h2 className="text-lg lg:text-2xl text-role-text font-semibold">
-          {dataAll.placementName}
-        </h2>
-        <h2 className="text-2xl lg:text-4xl mb-1 font-semibold">
-          {dataAll.companyName}
-        </h2>
-        <div className="text-md lg:text-xl py-2 leading-tight lg:leading-tight font-medium">
-          <p>{dataAll.description}</p>
-        </div>
-        <div className="flex flex-row item-center mt-4 lg:mt-5">
-          <h3 className="text-sm lg:text-lg font-medium">Roles:&nbsp;</h3>
-          {dataAll.roles.map((role) => {
+    <SkeletonTheme baseColor="#7C7B7B" highlightColor="#686868" height={25}>
+      <div className="flex items-center justify-center py-10 mb-10 h-auto bg-background-clr font-inter font-normal">
+        <section
+          className="flex flex-col p-5 sm:p-8 lg:p-16  w-11/12 sm:w-10/12 md:w-2/3 lg:w-3/5 border-white h-auto rounded-md bg-primary-card"
+          ref={widthRef}
+        >
+          <h2 className="text-lg lg:text-2xl text-role-text font-semibold">
+            {dataAll.placementNam || <Skeleton width={boxWidth / 1.92} />}
+          </h2>
+          <h2 className="text-2xl lg:text-4xl mb-1 font-semibold">
+            {dataAll.companyNam || <Skeleton width={boxWidth / 1.5} />}
+          </h2>
+          <div className="text-md lg:text-xl py-2 leading-tight lg:leading-tight font-medium">
+            <p>{dataAll.descriptio || <Skeleton height={80} />}</p>
+          </div>
+
+          {dataAll.role ? (
+            <div className="flex flex-row item-center mt-4 lg:mt-5">
+              <h3 className="text-sm lg:text-lg font-medium">Roles:&nbsp;</h3>
+              {dataAll.roles.map((role) => {
+                return (
+                  <p
+                    key={role.subID}
+                    className="bg-secondary-card text-role-text-2 rounded-md px-2 ml-2 text-sm lg:text-lg font-medium"
+                  >
+                    {role.roles}
+                  </p>
+                );
+              })}
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 1.5} />
+          )}
+
+          <hr className=" border-divider-color mt-5" />
+          {dataAll.roles.map((innerRole) => {
             return (
-              <p
-                key={role.subID}
-                className="bg-secondary-card text-role-text-2 rounded-md px-2 ml-2 text-sm lg:text-lg font-medium"
-              >
-                {role.role}
-              </p>
+              <RolesCard
+                key={innerRole.subID}
+                props={innerRole}
+                boxWidth={boxWidth}
+              />
             );
           })}
-        </div>
-        <hr className=" border-divider-color mt-5" />
-
-        {dataAll.roles.map((innerRole) => {
-          return <RolesCard key={innerRole.subID} props={innerRole} />;
-        })}
-      </section>
-    </div>
+        </section>
+      </div>
+    </SkeletonTheme>
   );
 };
 
--- client/src/components/RolesCard.jsx
+++ client/src/components/RolesCard.jsx
@@ -1,40 +1,77 @@
-import React from "react"
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const RolesCard = ({ props }) => {
+const RolesCard = ({ props, boxWidth }) => {
   return (
-    <section className="flex flex-col mt-8 text-sm lg:text-base">
-      <h2 className="text-xl lg:text-2xl font-medium">{props.role}</h2>
-      <div className="bg-secondary-card rounded-md px-3 py-4 sm:p-6 lg:p-6 font-medium">
-        {/* this is the description */}
-        <p className="mb-8 lg:mb-5 sm:leading-tight">{props.roleDescription}</p>
+    <SkeletonTheme baseColor="#7C7B7B" highlightColor="#686868" height={25}>
+      <section className="flex flex-col mt-8 text-sm lg:text-base">
+        <h2 className="mb-2 text-xl lg:text-2xl font-medium">
+          {props.rol || <Skeleton width={boxWidth / 1.92} />}
+        </h2>
+        <div className="bg-secondary-card rounded-md px-3 py-4 sm:p-6 lg:p-6 font-medium">
+          {/* this is the description */}
+          <p className="mb-8 lg:mb-5 sm:leading-tight">
+            {props.roleDescriptio || <Skeleton height={80} />}
+          </p>
 
-        <div className="lg:mt-3 mt-5 flex flex-row">
-          <h3>Qualifications:&nbsp;</h3>
-          <p className="text-role-text-2 ">{props.qualification}</p>
-        </div>
-        <div className="lg:mt-3 mt-5 flex flex-row">
-          <h3>Cutoff:&nbsp;</h3>
-          <p className="text-role-text-2">{props.cutoff}</p>
-        </div>
-        <div className="lg:mt-3 mt-5 flex flex-row">
-          <h3>CTC:&nbsp;</h3>
-          <p className="text-role-text-2">{props.ctc}</p>
-        </div>
-        <div className="lg:mt-3 mt-5 flex flex-row">
-          <h3>Stipend:&nbsp;</h3>
-          <p className="text-role-text-2">{props.stipend}</p>
-        </div>
-        <div className="lg:mt-3 mt-5 flex flex-row items-center">
-          <h3>Service Agreement:&nbsp;</h3>
-          <p className="text-role-text-2">{props.serviceAgreement}</p>
-        </div>
-        <div className="lg:mt-3 mt-5 flex flex-row">
-          <h3>Location:&nbsp;</h3>
-          <p className="text-role-text-2">{props.location}</p>
-        </div>
-      </div>
-    </section>
-  )
-}
+          {props.qualificatio ? (
+            <div className="lg:mt-3 mt-5 flex flex-row">
+              <h3>Qualifications:&nbsp;</h3>
+              <p className="text-role-text-2 ">{props.qualification}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
 
-export default RolesCard
+          {props.cutof ? (
+            <div className="lg:mt-3 mt-5 flex flex-row">
+              <h3>Cutoff:&nbsp;</h3>
+              <p className="text-role-text-2">{props.cutoff}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
+
+          {props.ct ? (
+            <div className="lg:mt-3 mt-5 flex flex-row">
+              <h3>CTC:&nbsp;</h3>
+              <p className="text-role-text-2">{props.ctc}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
+
+          {props.stipen ? (
+            <div className="lg:mt-3 mt-5 flex flex-row">
+              <h3>Stipend:&nbsp;</h3>
+              <p className="text-role-text-2">{props.stipend}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
+
+          {props.serviceAgreemen ? (
+            <div className="lg:mt-3 mt-5 flex flex-row items-center">
+              <h3>Service Agreement:&nbsp;</h3>
+              <p className="text-role-text-2">{props.serviceAgreement}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
+
+          {props.locatio ? (
+            <div className="lg:mt-3 mt-5 flex flex-row">
+              <h3>Location:&nbsp;</h3>
+              <p className="text-role-text-2">{props.location}</p>
+            </div>
+          ) : (
+            <Skeleton width={boxWidth / 2} />
+          )}
+        </div>
+      </section>
+    </SkeletonTheme>
+  );
+};
+
+export default RolesCard;
